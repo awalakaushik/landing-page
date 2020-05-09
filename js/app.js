020558eae4829cfa39b1821e67694e103e496edc
awalakaushik <awalakaushik@gmail.com>
2020-05-09 18:48:05 -0500

    build navigation bar using the sections in html

--- js/app.js
+++ js/app.js
@@ -18,14 +18,34 @@
  * 
 */
 
+const docFragment = document.createDocumentFragment();
 
+const navbar = document.getElementById('navbar__list');
+
+// convert the HTMLCollection Object to an Array using spread operator
+const sections = [...document.getElementsByTagName('section')];
+
+const navbarListNames = getNavbarListItems(sections);
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function getNavbarListItems(sections) {
+    return sections.map(section => {
+        return section.getAttribute('data-nav');
+    });
+}
 
-
+function buildNavBar(navbarListNames) {
+    navbarListNames.forEach(name => {
+        const listItem = document.createElement('li');
+        listItem.innerText = name;
+        listItem.className = 'menu__link';
+        docFragment.appendChild(listItem);
+    });
+    navbar.appendChild(docFragment);
+}
 
 /**
  * End Helper Functions
@@ -34,7 +54,7 @@
 */
 
 // build the nav
-
+buildNavBar(navbarListNames);
 
 // Add class 'active' to section when near top of viewport
 
