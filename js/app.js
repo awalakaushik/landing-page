a9678ab24406a8b6a8f8831a8dd6eb0a49ddb817
awalakaushik <awalakaushik@gmail.com>
2020-05-10 00:02:33 -0500

    add active class to scroll event

--- js/app.js
+++ js/app.js
@@ -37,18 +37,35 @@ function getNavbarListItems(sections) {
     });
 }
 
-function scrollAndMakeActive(event) {
-    
+function addActiveClass(newElement) {
     // remove active class on the current active section
-    document.querySelector(".active-section").classList.remove("active-section");    
-    
-    const newActiveSectionId = event.target.getAttribute('data-section-id').toLowerCase();
-    // scroll into view
-    const newActiveSection = document.getElementById(newActiveSectionId);
-    newActiveSection.scrollIntoView(false);
+    document.querySelector(".active-section").classList.remove("active-section");
 
     // add active class to the new section
-    newActiveSection.className = 'active-section';
+    newElement.className = 'active-section';
+}
+
+function scrollAndMakeActive(event) {
+    // scroll into view
+    const newSectionId = event.target.getAttribute('data-section-id');
+    const newSection = document.getElementById(newSectionId);
+    newSection.scrollIntoView(false);
+
+    // add active class to this new section
+    addActiveClass(newSection);
+
+}
+
+function scrollEventListener(event) {
+    
+	sections.forEach(section => {
+        if (!isInViewport(section)) {
+            return;
+        }
+        else {
+            addActiveClass(section);
+        }
+    })
 }
 
 function buildNavBar(navbarListNames) {
@@ -56,12 +73,28 @@ function buildNavBar(navbarListNames) {
         const listItem = document.createElement('li');
         listItem.innerText = name;
         listItem.className = 'menu__link';
-        listItem.setAttribute('data-section-id', name.replace(/\s/g, ''));
+        listItem.setAttribute('data-section-id', name.replace(/\s/g, '').toLowerCase());
         listItem.addEventListener('click', scrollAndMakeActive);
         docFragment.appendChild(listItem);
     });
     navbar.appendChild(docFragment);
 }
+
+/*!
+ * Determine if an element is in the viewport
+ * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}    elem The element
+ * @return {Boolean}      Returns true if element is in the viewport
+ */
+function isInViewport (elem) {
+	const distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
 
 /**
  * End Helper Functions
@@ -73,21 +106,5 @@ function buildNavBar(navbarListNames) {
 buildNavBar(navbarListNames);
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+window.addEventListener('scroll', scrollEventListener);
 
